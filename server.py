8f7ccd878893decfc8487e8a304dead4bf968816
Trew Boisvert <trewboisvert@gmail.com>
2023-04-07 15:31:11 -0700

    Add homepage route to server

--- server.py
+++ server.py
@@ -1,1 +1,14 @@
-#single page app
+from flask import Flask, render_template, jsonify, session, request
+
+app = Flask(__name__)
+
+app.secret_key = "damngoodmarmalade"
+
+@app.route("/")
+def homepage():
+    """Simple greeting page."""
+    
+    return render_template("homepage.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
